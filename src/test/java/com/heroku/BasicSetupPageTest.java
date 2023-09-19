59f815cd2702d06efde05a5d498195962bc76edc
rmargot7715 <rmargot7715@gmail.com>
2023-09-19 15:54:57 -0400

    Updated Selenium version in pom.xml and removed commented code

--- src/test/java/com/heroku/BasicSetupPageTest.java
+++ src/test/java/com/heroku/BasicSetupPageTest.java
@@ -5,11 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BasicSetupPageTest extends ConfigReader {
